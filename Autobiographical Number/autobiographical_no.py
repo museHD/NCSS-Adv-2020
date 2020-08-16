c34a559150f3866f52fc2f1d4187c8b1c2451e55
museHD <57558058+museHD@users.noreply.github.com>
2020-08-16 22:40:29 +1000

    wip - autobiographical

---
+++ Autobiographical Number/autobiographical_no.py
@@ -0,0 +1,24 @@
+def is_ab(num):
+	num_str = str(num)
+	freq = {}
+	revfreq = {}
+	# ↓ Going through the string and storing the frequency of each char in 'freq'
+	for digit in num_str:
+
+		revfreq[num_str.index(digit)] = digit
+
+		if digit in freq:
+			freq[digit] += 1
+		else:
+			freq[digit] = 1
+	print(freq)
+	print(revfreq)
+	for x in range(len(num_str)):
+
+		print(freq[str(x)])
+		print(revfreq.get(freq[str(x)]))
+		# 2 
+		if freq[str(x)] == revfreq.get(freq[str(x)]):
+			print('yech')
+
+is_ab(21200)
