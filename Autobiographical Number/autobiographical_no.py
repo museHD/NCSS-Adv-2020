1afb1fa6a956c7d5875d17c63050e51d2594074a
museHD <57558058+museHD@users.noreply.github.com>
2020-08-20 20:35:20 +1000

    Week 4 Rhyme Time

--- Autobiographical Number/autobiographical_no.py
+++ Autobiographical Number/autobiographical_no.py
@@ -13,12 +13,14 @@ def is_ab(num):
 			freq[digit] = 1
 	print(freq)
 	print(revfreq)
-	for x in range(len(num_str)):
+	# for x in range(len(num_str)):
 
-		print(freq[str(x)])
-		print(revfreq.get(freq[str(x)]))
-		# 2 
-		if freq[str(x)] == revfreq.get(freq[str(x)]):
-			print('yech')
+	# 	print(freq[str(x)])
+	# 	print(revfreq.get(freq[str(x)]))
+	# 	# 2 
+	# 	if freq[str(x)] == revfreq.get(freq[str(x)]):
+	# 		print('yech')
+
+	
 
 is_ab(21200)
